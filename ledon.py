f17b557cdeef5a28c49c9a37d53adf81fd98cc43
thh0003 <thh0003@mix.wvu.edu>
2022-04-11 21:59:51 -0500

    initial commit led control test

---
+++ ledon.py
@@ -0,0 +1,10 @@
+from gpiozero import LED 
+from time import sleep 
+
+red = LED(17) 
+ 
+while True: 
+    red.on()    #turn led on 
+    sleep(1)    #delay for 1 second 
+    red.off()   #turn led off 
+    sleep(1):
